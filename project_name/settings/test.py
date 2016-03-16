e1c5adee5124967f9b792fd598af60779271bc0d
Miguel Vieira <jmvieira@gmail.com>
2016-03-16 15:41:26 +0000

    Added test settings.

---
+++ project_name/settings/test.py
@@ -0,0 +1,23 @@
+from base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+SECRET_KEY = 'test'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'cache',
+    }
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    },
+}
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
