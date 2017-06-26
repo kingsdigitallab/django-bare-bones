10cce744d9e9636b40d306f654b3efb3d27c696c
Brian Maher <brian.maher@kcl.ac.uk>
2017-06-26 10:50:37 +0100

    Fixed: kdl_ldap import in urls, missing dependency for wagtail, typo in settings.__init__

--- project_name/settings/__init__.py
+++ project_name/settings/__init__.py
@@ -6,4 +6,4 @@
 # imported here. Use it for settings specific to the installation and do not
 # commit to version control.
 # -----------------------------------------------------------------------------
-from local import * # oqa
+from local import * # noqa
