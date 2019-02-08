aaa582c782eb785680519b27118d774f8f654ba4
Geoffroy Noël <geoffroy-noel-ddh@users.noreply.github.com>
2019-02-08 19:14:54 +0000

    deploy js/css libs with "npm ci" in fabfile

--- fabfile.py
+++ fabfile.py
@@ -330,7 +330,7 @@ def collect_static(process=False):
     require('srvr', 'path', 'within_virtualenv', provided_by=env.servers)
 
     with cd(env.path):
-        run('npm i')
+        run('npm ci')
 
     if env.srvr in ['local', 'vagrant']:
         print(yellow('Do not run collect_static on local servers'))
