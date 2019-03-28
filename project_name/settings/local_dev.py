ab2ba848842e1119373e5f44968d2dc4f40aba57
Brian Maher <brian.maher@kcl.ac.uk>
2019-03-28 13:04:20 +0000

    Fix typo in local_dev.py

--- project_name/settings/local_dev.py
+++ project_name/settings/local_dev.py
@@ -1,7 +1,7 @@
 from .base import *  # noqa
 
 ALLOWED_HOSTS = ['127.0.0.1',
-                 'localhost'
+                 'localhost',
                  '[::1]',
                  '$PROJECT_NAME-dev.kdl.kcl.ac.uk']
 
